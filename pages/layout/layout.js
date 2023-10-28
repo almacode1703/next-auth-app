6999d8e36eb41e4a8c5e4ee53eb0618467387201
almacode1703 <testmygitt.2023@gmail.com>
2023-10-28 13:33:25 +0530

    first commit

--- pages/layout/layout.js
+++ pages/layout/layout.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import styles from '../../styles/layout.module.css'
 
 export default function Layout({ children }) {
     return (
         <div className='flex h-screen bg-blue-400'>
             <div className='m-auto bg-slate-50 rounded-md w-3/5 h-3/4 grid lg:grid-cols-2'>
                 images
-                <div className='right flex flex-col justify-evenly'>
+                <div className='right flex flex-col justify-evenly bg-gray-500'>
                     <div className='text-center py-10'>
                         {children}
                     </div>
