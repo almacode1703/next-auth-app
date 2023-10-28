b3248002aa66732a8282f998a888fba421541219
almacode1703 <testmygitt.2023@gmail.com>
2023-10-28 16:45:13 +0530

    more styles added

--- pages/layout/layout.js
+++ pages/layout/layout.js
@@ -5,8 +5,10 @@ export default function Layout({ children }) {
     return (
         <div className='flex h-screen bg-blue-400'>
             <div className='m-auto bg-slate-50 rounded-md w-3/5 h-3/4 grid lg:grid-cols-2'>
-                images
-                <div className='right flex flex-col justify-evenly bg-gray-500'>
+                <div className={styles.imgStyle}>
+                    <div className= {styles.loginImage}></div>
+                </div>
+                <div className='right flex flex-col justify-evenly '>
                     <div className='text-center py-10'>
                         {children}
                     </div>
--- pages/login/index.jsx
+++ pages/login/index.jsx
@@ -8,7 +8,31 @@ export default function Login() {
             <Head>
                 <title>Login</title>
             </Head>
-            <h1>Login</h1>
+            <section className='w-3/4 mx-auto flex flex-col gap-10'>
+
+                <div className="title">
+                    <h1 className='text-grey-800 text-4xl font-bold py-4'>Explore</h1>
+                    <p className='w-5/6 mx-auto text-gray-400'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora, molestias?</p>
+                </div>
+
+                <form className='flex flex-col gap-5'>
+                    <div className="input-group">
+                        <input type="email" name='email' placeholder='john.doe@gmail.com' />
+                    </div>
+                    <div className="input-group">
+                        <input type="password" name='password' placeholder='password' />
+                    </div>
+                    <div className="input-button">
+                        <button type="submit">Login</button>
+                    </div>
+                    <div className="input-button">
+                        <button type="submit">Sign In with Google</button>
+                    </div>
+                    <div className="input-button">
+                        <button type="submit">Sign In with Github</button>
+                    </div>
+                </form>
+            </section>
         </Layout>
     )
 }
