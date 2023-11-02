73766dda2ba5790bc883cc6ba5de03a1dcda8bb6
almacode1703 <testmygitt.2023@gmail.com>
2023-11-02 10:51:59 +0530

    login functionality implemented

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,10 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from "next-auth/providers/google"
 import GithubProvider from "next-auth/providers/github"
+import { CredentialsProvider } from 'next-auth/providers/credentials'
+import connectMongo from '@/database/conn'
+import Users from '@/model/Schema'
+import {compare} from 'bcryptjs'
 
 export default NextAuth ({
    providers: [
@@ -14,6 +18,29 @@ export default NextAuth ({
     GithubProvider({
         clientId : process.env.GITHUB_ID,
         clientSecret: process.env.GITHUB_SECRET
+    }),
+    // Credential provider
+    CredentialsProvider({
+      name : "Credentials",
+      async authorize(credentials, req){
+        try {
+            await connectMongo()
+        }
+        catch(error){
+            console.log(error)
+        }
+
+        const result = await Users.findOne({email : credentials.email})
+        if(!result){
+            throw new Error("No User found...")
+        }
+
+        // compare()
+        const checkPassword = await compare(credentials.password, result.password)
+        if((!checkPassword) || (result.email !== credentials.email)){
+             throw new Error("Username or Password doesn't match")
+        } 
+      }
     })
    ]
 })
